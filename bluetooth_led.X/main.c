2f8c9c4d5762501320fa1c788767e42c1689584d
Verónica Fernández Pozo <vfdezpozo@gmail.com>
2014-11-14 11:14:45 +0100

    Xc8

--- bluetooth_led.X/main.c
+++ bluetooth_led.X/main.c
@@ -8,7 +8,8 @@
 #include "main.h"
 
 char CaracterRx;
-char MensajeTx[]="COMUNICACION SATISFACTORIA";
+char MensajeTx[]="HOLA\n\r";
+
 
 void x_delay_ms(int x) {
     int cont = x/10;
@@ -25,7 +26,7 @@ void main(void) {
     CloseUSART(); //turn off usart if was previously on
     // --- configure USART
     unsigned char config = USART_TX_INT_OFF & USART_RX_INT_ON & USART_ASYNCH_MODE & USART_EIGHT_BIT & USART_CONT_RX&USART_BRGH_HIGH;
-    unsigned int spbrg = 624; //At 48MHz of oscillator frequency & baud rate of 9600
+    unsigned int spbrg = 103; //At 48MHz of oscillator frequency & baud rate of 115200
     OpenUSART(config, spbrg); //API configures USART for desired parameters
 
     unsigned char baudconfig = BAUD_16_BIT_RATE&BAUD_AUTO_OFF;
@@ -35,22 +36,20 @@ void main(void) {
     INTCONbits.GIE=1;//interrupciones globales
 
     while(BusyUSART());
-    putsUSART(MensajeTx);
+    putsUSART(&MensajeTx);
+
 
     while (1) {
-        LED_RED_Toggle();
         x_delay_ms(250);
         LED_GREEN_Toggle();
-
     }
 }
 
 void interrupt Interrupcion(){
     CaracterRx=ReadUSART();
+    LED_RED_Toggle();
     while(BusyUSART());
-    putsUSART("\n\rEnviado: ");
-    while(BusyUSART());
-    WriteUSART(CaracterRx);
+    putsUSART(&CaracterRx);
     PIR1bits.RCIF=0;
 }
 
